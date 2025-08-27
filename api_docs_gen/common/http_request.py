6daa639ceac6ac99de2470704882e75a420da6a5
AnkurMPansara <ankur.at.surat@gmail.com>
2025-08-27 10:21:30 +0530

    Changes

---
+++ api_docs_gen/common/http_request.py
@@ -0,0 +1,39 @@
+import http.client
+import json
+from urllib.parse import urlparse
+
+def makeHttpRequest(apiUrl: str, method: str, headers: dict = {}, payload: dict = {}, timeout: int = 3000) -> dict:
+    headers = headers or {}
+    parsedUrl = urlparse(apiUrl)
+
+    if parsedUrl.scheme == "https":
+        conn = http.client.HTTPSConnection(parsedUrl.netloc, timeout=timeout)
+    else:
+        conn = http.client.HTTPConnection(parsedUrl.netloc, timeout=timeout)
+
+    requestBody = json.dumps(payload) if payload else None
+    if requestBody:
+        headers.setdefault("Content-Type", "application/json")
+    
+    try:
+        conn.request(method.upper(), parsedUrl.path, requestBody, headers)
+        resp = conn.getresponse()
+        respData = resp.read().decode()
+        
+        try:
+            apiResponse = json.loads(respData)
+        except json.JSONDecodeError:
+            pass
+        return {
+            "code" : resp.status,
+            "status" : "Failure",
+            "response" : apiResponse
+        }
+    except Exception as e:
+        return {
+            "code" : resp.status,
+            "status" : "Failure",
+            "response" : None
+        }
+    finally:
+        conn.close()
---
+++ api_docs_gen/llm/send_request.py
@@ -0,0 +1,3 @@
+from ..common import http_request
+
+def sendRequest(payload: dict) -> dict:
